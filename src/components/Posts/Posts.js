0136683980587a320f7775d731101ff75f9599bf
JS MacBook Pro <jsmacbookpro@JSs-MacBook-Pro.local>
2020-11-20 22:11:49 -0600

    completes tasks in App.js

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -3,6 +3,7 @@ import Post from './Post';
 import './Posts.css';
 
 const Posts = (props) => {
+  console.log('Posts props: ', props)
   // 🔥 Make sure the parent of Posts is passing the right props!
   const { likePost, posts } = props;
 
