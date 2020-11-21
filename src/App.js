07aaffcba35eaa5225a68dfcb04ca28867b5b3e7
JS MacBook Pro <jsmacbookpro@JSs-MacBook-Pro.local>
2020-11-20 22:40:16 -0600

    adds onClick handler that utilizes likePost in LikeSection.js

--- src/App.js
+++ src/App.js
@@ -47,9 +47,12 @@ const App = () => {
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Post />
+      <Post
+      post={posts}
+      likePost={likePost}
+      />
       <Posts 
-        post={posts}
+        posts={posts}
         likePost={likePost} 
       />
 
